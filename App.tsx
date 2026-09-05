b16b34708fd1123f35743f2f1642082ae9ad8713
Imtiaz Ali <imtiazali17314@gmail.com>
2026-09-05 17:22:58 +0500

    chore: update dependencies in package.json
    - Added new dependencies:
      - @react-native-async-storage/async-storage: ^3.1.1
      - @react-navigation/native: ^7.3.18
      - @react-navigation/native-stack: ^7.18.10
      - axios: ^1.20.0
      - lucide-react-native: ^1.41.0
    
    - Updated existing dependencies:
      - react-native-safe-area-context: ^5.5.2 to ^5.9.1
      - react-native-screens: added ^4.27.0
      - react-native-svg: added ^15.15.5
    
    - Removed duplicate entry for @react-native/new-app-screen

--- App.tsx
+++ App.tsx
@@ -1,14 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
 import {
   SafeAreaProvider,
+  SafeAreaView,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 
@@ -18,21 +17,12 @@ function App() {
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+      <SafeAreaView style={styles.container}>
+        <Text style={{ color: isDarkMode ? 'white' : 'black' }}>
+          ExPense Manager App
+        </Text>
+      </SafeAreaView>
     </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
   );
 }
 
