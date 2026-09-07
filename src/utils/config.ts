3d553785ec020e3c458c129b6952ac1b67636afa
Imtiaz Ali <imtiazali17314@gmail.com>
2026-09-07 12:18:49 +0500

    feat: update API_BASE_URL to use dynamic IP from ips.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,11 +1,7 @@
+import { ip } from '../../ips';
+
 // Android Emulator ke liye 10.0.2.2,
 // export const API_BASE_URL = 'http://10.0.2.2:8000/api';
 
-//  KPSIAJ Laibrary
-export const API_BASE_URL = 'http://192.168.100.49:8000/api';
-
-// Home
-// export const API_BASE_URL = 'http://192.168.100.49:8000/api';
-
-// Abbas Home
-// export const API_BASE_URL = 'http://192.168.100.49:8000/api';
+//  Connected Mobile ke liye
+export const API_BASE_URL = `${ip}:8000/api`;
